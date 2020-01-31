1af8f3c8c0c746e9bd776731f436d34a92aa75dd
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-01-31 18:41:38 +0100

    moved update back to index it needs db

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -24,9 +24,9 @@ class Module {
     /**
      * Module Version
      *
-     * @since 1.0.6
+     * @since 1.0.6.1
      */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.6.1';
 
     public function getConfig() : array {
         return include __DIR__ . '/../config/module.config.php';
