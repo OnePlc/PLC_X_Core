9f2e0ba8f3d10717aaa092265901db593f4dd93c
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-02-12 05:27:31 +0100

    release

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -27,7 +27,7 @@ class Module {
      *
      * @since 1.0.0
      */
-    const VERSION = '1.0.20';
+    const VERSION = '1.0.21';
 
     public function getConfig() : array {
         return include __DIR__ . '/../config/module.config.php';
